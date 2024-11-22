c8304249df906173adb8c564b20e8ed422da6d6c
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-11-22 09:57:56 +0100

    ProyectoCompletado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const lista = [
+    { Elemento : 'Marisco'},
+    { Elemento : 'Cerdo'},
+    { Elemento : 'Cecina'},
+    { Elemento : 'Pescado'},
+    { Elemento : 'Frutas'},
+    { Elemento : 'Pasteles'}
+  ];
+
+  const handleChange = (event) => {
+    setNombre(event.target.value);
+  };
+
+  const listaFiltrada = lista.filter((item) => {
+    return item.Elemento.toLowerCase().startsWith(nombre.toLowerCase());
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Lista de productos:</p>
+        <input type="text"  value={nombre} onChange={handleChange} placeholder="Filtrar por nombre"/>
+        <div>
+          {listaFiltrada.length > 0 ? (
+            listaFiltrada.map((item, index) => (
+              <div key={index}>
+                <p>{item.Elemento}</p>
+              </div>
+            ))
+          ) : (
+            <p>No se contraron elementos.</p>
+          )}
+        </div>
       </header>
     </div>
   );
